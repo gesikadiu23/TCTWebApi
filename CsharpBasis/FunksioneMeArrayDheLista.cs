6fc42e06457b434ef5b04f7d590390671ed28901
gesikadiutct <kadiugesi@gmail.com>
2023-02-14 20:55:38 +0100

    Ndryshuam ciklin for ne foreach

--- CsharpBasis/FunksioneMeArrayDheLista.cs
+++ CsharpBasis/FunksioneMeArrayDheLista.cs
@@ -8,29 +8,43 @@ namespace CsharpBasis
 {
     internal class FunksioneMeArrayDheLista
     {
-        public int GjejMeTeMadhin(int[] array){
+        public int GjejMeTeMadhin(int[] array) {
 
             int max = 0;
 
-            for(int i = 0; i < array.Length; i++)
+            foreach (int item in array)
             {
-                if (array[i] > max) max = array[i];
+                if (item > max) max = item;
             }
 
             return max;
         }
 
         public int GjejShumenEArray(int[] array) {
-            
+
             int sum = 0;
 
-            for (int i = 0; i< array.Length; i++)
+            foreach(int item in array)
             {
-                sum = sum + array[i];
+                sum = sum + item;
             }
-            
-            return sum;
 
+            return sum;
         }
+
+        public int GjejShumenCift(int[] array)
+        {
+            int sum = 0;
+
+            foreach(int item in array)
+            {
+                if(item % 2 == 0)
+                {
+                    sum = sum + item;
+                }
+            }
+            return sum;
+        }
+
     }
 }
--- CsharpBasis/Program.cs
+++ CsharpBasis/Program.cs
@@ -3,9 +3,7 @@ using CsharpBasis;
 
 Console.WriteLine("Hello, World!");
 
-
 int a;
-
 
 FunksioneMeArrayDheLista instanca = new FunksioneMeArrayDheLista();
 
@@ -19,6 +17,8 @@ n = Convert.ToInt32(Console.ReadLine());
 
 int[] array = new int[n];
 
+
+
 // Kemi mbushur arrayn me elemente
 for(int i = 0; i < n; i++)
 {
@@ -27,9 +27,16 @@ for(int i = 0; i < n; i++)
 }
 
 
+
+
 int shuma  = instanca.GjejShumenEArray(array);
 int meIMadhi = instanca.GjejMeTeMadhin(array);
+int shumaCift = instanca.GjejShumenCift(array);
 
 
 Console.WriteLine("Shuma: " + shuma);
 Console.WriteLine("Me i madhi: " + meIMadhi);
+Console.WriteLine("Shuma cift: " + shumaCift);
+
+
+
