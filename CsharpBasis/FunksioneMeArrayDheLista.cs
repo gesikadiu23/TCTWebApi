90dc0e12985e24230a076e194d437f873994c1fd
gesikadiutct <kadiugesi@gmail.com>
2023-02-14 19:48:12 +0100

    Shtuam nje klase me dy funksione per array

---
+++ CsharpBasis/FunksioneMeArrayDheLista.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CsharpBasis
+{
+    internal class FunksioneMeArrayDheLista
+    {
+        public int GjejMeTeMadhin(int[] array){
+
+            int max = 0;
+
+            for(int i = 0; i < array.Length; i++)
+            {
+                if (array[i] > max) max = array[i];
+            }
+
+            return max;
+        }
+
+        public int GjejShumenEArray(int[] array) {
+            
+            int sum = 0;
+
+            for (int i = 0; i< array.Length; i++)
+            {
+                sum = sum + array[i];
+            }
+            
+            return sum;
+
+        }
+    }
+}
