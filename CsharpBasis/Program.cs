574b37d8b1180dbcc9244ee075cbd2dbfbbdd3d5
gesikadiutct <kadiugesi@gmail.com>
2023-02-14 20:25:29 +0100

    Perdorem funksionet e shtuara ne program.cs

--- CsharpBasis/Program.cs
+++ CsharpBasis/Program.cs
@@ -1,4 +1,35 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using CsharpBasis;
+
 Console.WriteLine("Hello, World!");
 
 
+int a;
+
+
+FunksioneMeArrayDheLista instanca = new FunksioneMeArrayDheLista();
+
+
+Console.WriteLine("Ju lutem jepni nje array me int :");
+
+int n;
+
+Console.WriteLine("Sa doni te jete madhesia e array?");
+n = Convert.ToInt32(Console.ReadLine());
+
+int[] array = new int[n];
+
+// Kemi mbushur arrayn me elemente
+for(int i = 0; i < n; i++)
+{
+    Console.WriteLine("Jepni elementin ne pozicioinin: " + i);
+    array[i] = Convert.ToInt32(Console.ReadLine());
+}
+
+
+int shuma  = instanca.GjejShumenEArray(array);
+int meIMadhi = instanca.GjejMeTeMadhin(array);
+
+
+Console.WriteLine("Shuma: " + shuma);
+Console.WriteLine("Me i madhi: " + meIMadhi);
